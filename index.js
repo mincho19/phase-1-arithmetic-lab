1cd6c1b3f347a4d5f05a5684cf0def8eef9c10cd
Min Cho <minsangcho19@gmail.com>
2021-12-13 17:00:58 -0800

    commit tree ae457a5a495b7ab95b80aa74ea83ceae427c6cb3

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2; 
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor((Math.random()*10))+1;
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3%num4;
+
+let max = math.max(1,2,20);
+
+
